8ba836e8670d823f9dc31bf07f0327ae2db540eb
mcgirjau <mgirjau21@amherst.edu>
2019-10-12 16:01:54 -0400

    Add tests for CaesarShiftCipher and fix bug

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,21 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.sql.SQLOutput;
+
 public class CaesarShiftCipherTest {
 
+    @ParameterizedTest(name = "Message is {0}, shift is {1}, and translation is {2}")
+    @CsvSource({"abc, 3, def", "olssv, 757, rovvy", "2nkjk, 2, invalid",
+                "nkjk4, 1, invalid", "babkej, -10, rqrauz", "abc def, 1, bcd efg"})
+    public void shift(String message, int shift, String translation) {
+
+        CaesarShiftCipher myCipher = new CaesarShiftCipher();
+        String result = myCipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(translation, result);
+
+    }
 }
