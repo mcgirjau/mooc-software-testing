4d3ff7222fb323b8899143c74f20e4e552e76101
mcgirjau <mgirjau21@amherst.edu>
2019-10-12 15:34:34 -0400

    Add testing methods for GHappy and fix bug

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,62 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+
+    public GHappy myString;
+
+    @BeforeEach
+    public void initialize() {
+        this.myString = new GHappy();
+    }
+
+    @Test
+    public void hasOnlyGBeginning() {
+
+        boolean result = myString.gHappy("gglkansdf");
+        Assertions.assertTrue(result);
+
+        result = myString.gHappy("gaaaaa");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void hasOnlyGEnd() {
+
+        boolean result = myString.gHappy("dlnfgg");
+        Assertions.assertTrue(result);
+
+        result = myString.gHappy("jjdjfjfdjg");
+        Assertions.assertFalse(result);
+
+    }
+
+    @Test
+    public void hasOnlyGMiddle() {
+
+        boolean result = myString.gHappy("adfgglkhrggdfa");
+        Assertions.assertTrue(result);
+
+        result = myString.gHappy("dlfknsgkjdnfggghj");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void hasGAnywhere() {
+        boolean result = myString.gHappy("ggaffgds");
+        Assertions.assertFalse(result);
+
+        result = myString.gHappy("sgkggghg");
+        Assertions.assertFalse(result);
+
+        result = myString.gHappy("ggggahggsdfgg");
+        Assertions.assertTrue(result);
+
+        result = myString.gHappy("ggagg");
+        Assertions.assertTrue(result);
+    }
+
 }
